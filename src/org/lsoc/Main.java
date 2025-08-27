981c9169d0761fcd0c8f2593117d1622aeea9b7b
krosen123456 <krosen@lsoc.org>
2025-08-27 16:31:28 +0000

    added my story to the code

--- src/org/lsoc/Main.java
+++ src/org/lsoc/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("This summer, I went to Costa Rica with my family and it was really fun. We went to the beach and also went on hikes in the rainforest.");
     }
 }
